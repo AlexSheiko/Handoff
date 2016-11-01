3dfff896e2249c0e7e4d4e227899ccd2dce6d757
AlexeySheiko <alexeysheiko@hotmail.com>
2016-11-01 17:15:23 +0100

    Make text in tabs smaller

--- app/src/main/java/com/example/handoff/MainActivity.java
+++ app/src/main/java/com/example/handoff/MainActivity.java
@@ -1,22 +1,20 @@
 package com.example.handoff;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -142,19 +140,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            // Show 4 total pages.
+            return 4;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Home";
                 case 1:
-                    return "SECTION 2";
+                    return "Community";
                 case 2:
-                    return "SECTION 3";
+                    return "On Sale";
+                case 3:
+                    return "In Progress";
             }
             return null;
         }
