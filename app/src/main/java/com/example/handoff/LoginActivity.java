69cba03166f9584112284e887faf023a3ba5d4a9
AlexeySheiko <alexeysheiko@hotmail.com>
2016-11-07 17:54:12 +0100

    Make status bar on the login screen transparent

--- app/src/main/java/com/example/handoff/LoginActivity.java
+++ app/src/main/java/com/example/handoff/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.handoff;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 }
