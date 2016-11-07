0572856bb77b7ddc3315af3f148900250fb7b41e
AlexeySheiko <alexeysheiko@hotmail.com>
2016-11-07 17:14:13 +0100

    Add price to the detail view

--- app/src/main/java/com/example/handoff/PickupDetailActivity.java
+++ app/src/main/java/com/example/handoff/PickupDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.handoff;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class PickupDetailActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class PickupDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pickup_detail);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
