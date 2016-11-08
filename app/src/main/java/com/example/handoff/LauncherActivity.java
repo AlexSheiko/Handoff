ca2884bf48506a3aeff2fdf33211562aabeec51e
AlexeySheiko <alexeysheiko@hotmail.com>
2016-11-08 22:06:33 +0100

    Animate sign up button

--- app/src/main/java/com/example/handoff/LauncherActivity.java
+++ app/src/main/java/com/example/handoff/LauncherActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
@@ -23,9 +24,12 @@ public class LauncherActivity extends AppCompatActivity {
 
     public void onClickSignUp(View view) {
         View logoView = findViewById(R.id.logoView);
+        View signUpButton = findViewById(R.id.signUpButton);
         Intent intent = new Intent(this, RegisterActivity.class);
         ActivityOptionsCompat options = ActivityOptionsCompat.
-                makeSceneTransitionAnimation(this, logoView, "logo");
+                makeSceneTransitionAnimation(this,
+                        new Pair<>(logoView, "logo"),
+                        new Pair<>(signUpButton, "button"));
         startActivity(intent, options.toBundle());
     }
 }
