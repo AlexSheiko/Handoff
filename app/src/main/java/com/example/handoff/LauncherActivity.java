72dbd75f0181fc63c2526012acac99029e4bc7d8
AlexeySheiko <alexeysheiko@hotmail.com>
2016-11-08 21:50:12 +0100

    Add launcher activity

---
+++ app/src/main/java/com/example/handoff/LauncherActivity.java
@@ -0,0 +1,20 @@
+package com.example.handoff;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+}
