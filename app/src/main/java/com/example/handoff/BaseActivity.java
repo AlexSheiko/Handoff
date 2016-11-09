acc14c539805178d11bb1a58ece4e3c162d5e2d4
AlexeySheiko <alexeysheiko@hotmail.com>
2016-11-09 19:08:46 +0100

    Do not override BaseActivity transition

--- app/src/main/java/com/example/handoff/BaseActivity.java
+++ app/src/main/java/com/example/handoff/BaseActivity.java
@@ -1,7 +1,6 @@
 package com.example.handoff;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -14,17 +13,5 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
-
-    @Override
-    public void startActivity(Intent intent) {
-        super.startActivity(intent);
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-    }
-
-    @Override
-    public void finish() {
-        super.finish();
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 }
