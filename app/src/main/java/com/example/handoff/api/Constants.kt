58445c4d67ebc5d2f412835e12c589015ba38a4b
AlexeySheiko <alexeysheiko@hotmail.com>
2016-12-26 16:44:56 +0100

    Set auth credentials as default for TokenRequest

--- app/src/main/java/com/example/handoff/api/Constants.kt
+++ app/src/main/java/com/example/handoff/api/Constants.kt
@@ -2,12 +2,12 @@ package com.example.handoff.api
 
 object Constants {
 
-    val CLIENT_DEFAULT = "3"
     val CLIENT_AUTH = "2"
+    val CLIENT_COMMON = "3"
 
-    val SECRET_DEFAULT = "nm89icirRGCrxUctufd23TSSgfhquB0PT71xY8AU"
     val SECRET_AUTH = "ZjE06oApwHzlqVMBCM4m1R0XnfbLb4AgyMX9HGns"
+    val SECRET_COMMON = "nm89icirRGCrxUctufd23TSSgfhquB0PT71xY8AU"
 
-    val GRANT_DEFAULT = "client_credentials"
     val GRANT_AUTH = "password"
+    val GRANT_COMMON = "client_credentials"
 }
--- app/src/main/java/com/example/handoff/api/model/TokenRequest.kt
+++ app/src/main/java/com/example/handoff/api/model/TokenRequest.kt
@@ -1,13 +1,13 @@
 package com.example.handoff.api.model
 
-import com.example.handoff.api.Constants.CLIENT_DEFAULT
-import com.example.handoff.api.Constants.GRANT_DEFAULT
-import com.example.handoff.api.Constants.SECRET_DEFAULT
+import com.example.handoff.api.Constants.CLIENT_AUTH
+import com.example.handoff.api.Constants.GRANT_AUTH
+import com.example.handoff.api.Constants.SECRET_AUTH
 
 data class TokenRequest(
-        val client_id: String = CLIENT_DEFAULT,
-        val client_secret: String = SECRET_DEFAULT,
-        val grant_type: String = GRANT_DEFAULT,
+        val client_id: String = CLIENT_AUTH,
+        val client_secret: String = SECRET_AUTH,
+        val grant_type: String = GRANT_AUTH,
         val username: String,
         val password: String,
         val scope: String)
