97566e90fd85ea4bee37e6e6267d08b7cf104db3
AlexeySheiko <alexeysheiko@hotmail.com>
2016-12-28 11:23:11 +0100

    Log Retrofit requests

--- app/src/main/java/com/example/handoff/api/ServiceGenerator.kt
+++ app/src/main/java/com/example/handoff/api/ServiceGenerator.kt
@@ -1,9 +1,12 @@
 package com.example.handoff.api
 
 import com.example.handoff.api.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 class ServiceGenerator {
     companion object {
@@ -15,10 +18,18 @@ class ServiceGenerator {
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .client(loggingClient())
                     .build()
             val service = restAdapter.create(clazz)
 
             return service
+        }
+
+        private fun loggingClient(): OkHttpClient? {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+            return client
         }
     }
 }
