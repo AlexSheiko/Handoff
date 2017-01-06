ee769538ddca6767e20ddfdbae9969354403e3a5
AlexeySheiko <alexeysheiko@hotmail.com>
2017-01-06 12:03:21 +0100

    Get orders from api in data manager

---
+++ app/src/main/java/com/example/handoff/data/DataManager.kt
@@ -0,0 +1,23 @@
+package com.example.handoff.data
+
+import com.example.handoff.api.ServiceGenerator.orderService
+import com.example.handoff.data.model.Order
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers.mainThread
+import rx.schedulers.Schedulers.io
+
+class DataManager {
+
+    fun getOrders(): Observable<List<Order>> {
+
+        val ordersApi = getOrdersFromApi()
+
+        return ordersApi
+                .observeOn(mainThread())
+    }
+
+    private fun getOrdersFromApi(): Observable<List<Order>> {
+        return orderService.orders()
+                .subscribeOn(io())
+    }
+}
