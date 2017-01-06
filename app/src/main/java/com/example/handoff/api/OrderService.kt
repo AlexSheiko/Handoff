ee2966cf54da396ac32bdb741a25a0f2950c3668
AlexeySheiko <alexeysheiko@hotmail.com>
2017-01-06 13:42:09 +0100

    Add authorization header to the orders() web call

--- app/src/main/java/com/example/handoff/api/OrderService.kt
+++ app/src/main/java/com/example/handoff/api/OrderService.kt
@@ -2,10 +2,13 @@ package com.example.handoff.api
 
 import com.example.handoff.data.model.Order
 import retrofit2.http.GET
+import retrofit2.http.Header
 import rx.Observable
 
 interface OrderService {
 
     @GET("api/v1/deliveries")
-    fun orders(): Observable<List<Order>>
+    fun orders(
+            @Header("Authorization") auth: String
+    ): Observable<List<Order>>
 }
